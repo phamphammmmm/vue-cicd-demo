40e8875cfcd04f96e26e92e42eb61d0aba729314
phamphammmmm <phamtiendat246@gmail.com>
2024-07-14 22:47:02 +0700

    add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 
 
 # Dừng các container đang chạy
-sudo docker-compose stop
+docker-compose stop
 
 # Xóa các container cũ
-sudo docker-compose rm -f
+docker-compose rm -f
 
 # Pull image mới từ Docker Hub
-sudo docker-compose pull
+docker-compose pull
 
 # Chạy container mới
-sudo docker-compose up -d
+docker-compose up -d
